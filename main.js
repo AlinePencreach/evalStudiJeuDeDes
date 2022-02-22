445e5246360f8f2fad0ec1d4734fa4d8b687128a
AlinePencreach <64145119+AlinePencreach@users.noreply.github.com>
2022-02-22 13:46:06 +0100

    redaction des commentaire pour organisation

--- main.js
+++ main.js
@@ -1,15 +1,21 @@
 /* déclarer le variable :
-- score global j1
-- score global j2
-- score joueur1
-- score joueur2
-- tour du joueur
-- resultat dés
+joueur 1 id = "player1"
+joueur 2 id = "player2"
+- score global j1 = class = "globalPlayer"
+
+- score joueur1 = id = "roundPlayer1"
+
+- score joueur2 = id = "roundPlayer2"
+
+- tour du joueur = active ? affiche point rouge
+
+- resultat dés = diceFaces
 
 bouton pour Evenement :
-- nouvelle partie
-- lancer
-- collecter */
+- nouvelle partie id : btnNewGame
+- lancer : id="btnLancer"
+- collecter id="btnCollecter"
+*/
 
 
 
