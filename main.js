ae90960a38e1da2521b700a8dccda151d8fc8284
AlinePencreach <64145119+AlinePencreach@users.noreply.github.com>
2022-02-22 14:05:16 +0100

    déclaration des variables

--- main.js
+++ main.js
@@ -1,21 +1,38 @@
-/* déclarer le variable :
-joueur 1 id = "player1"
-joueur 2 id = "player2"
-- score global j1 = class = "globalPlayer"
+// /* déclarer le variable :
+// joueur 1 id = "player1"
+let player1 = document.querySelector('#player1');
 
-- score joueur1 = id = "roundPlayer1"
+// joueur 2 id = "player2"
+let player2 = document.querySelector('#player2');
 
-- score joueur2 = id = "roundPlayer2"
+// - score global j1 = class = "globalPlayer"
+let global = document.querySelector('globalPlayer');
 
-- tour du joueur = active ? affiche point rouge
+// - score joueur1 = id = "roundPlayer1"
+let roundPlayer1 = document.querySelector('#roundPlayer1');
 
-- resultat dés = diceFaces
+// - score joueur2 = id = "roundPlayer2"
+let roundPlayer2 = document.querySelector('#roundPlayer2');
 
-bouton pour Evenement :
-- nouvelle partie id : btnNewGame
-- lancer : id="btnLancer"
-- collecter id="btnCollecter"
-*/
+
+// - tour du joueur
+let activePlayer; //ajouter rond rouge player active
+
+// - resultat dés = diceFaces
+let diceFaces; // comment lier les imgaes des dès ???
+
+
+// bouton pour Evenement :
+
+// - nouvelle partie id : btnNewGame
+let newGame = document.querySelector('#btnNewGame');
+
+// - lancer : id="btnLancer"
+let lancer = document.querySelector('#btnLancer');
+
+// - collecter id="btnCollecter"
+let collecter = document.querySelector('#btnCollecter');
+
 
 
 
