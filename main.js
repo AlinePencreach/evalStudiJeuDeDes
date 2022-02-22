0e52694c221b5a8610299e4958dd1cab01523874
AlinePencreach <64145119+AlinePencreach@users.noreply.github.com>
2022-02-22 14:12:55 +0100

    déclaration des variables

--- main.js
+++ main.js
@@ -1,18 +1,20 @@
 // /* déclarer le variable :
 // joueur 1 id = "player1"
-let player1 = document.querySelector('#player1');
+let player1 = document.getElementById('player1');
 
 // joueur 2 id = "player2"
-let player2 = document.querySelector('#player2');
+let player2 = document.getElementById('player2');
 
 // - score global j1 = class = "globalPlayer"
-let global = document.querySelector('globalPlayer');
+let global = document.getElementsByClassName('globalPlayer');
+let globalPlayer1 = 0;
+let globalPlayer2 = 0;
 
 // - score joueur1 = id = "roundPlayer1"
-let roundPlayer1 = document.querySelector('#roundPlayer1');
+let roundPlayer1 = document.getElementsById('roundPlayer1');
 
 // - score joueur2 = id = "roundPlayer2"
-let roundPlayer2 = document.querySelector('#roundPlayer2');
+let roundPlayer2 = document.getElementById('roundPlayer2');
 
 
 // - tour du joueur
@@ -25,13 +27,13 @@ let diceFaces; // comment lier les imgaes des dès ???
 // bouton pour Evenement :
 
 // - nouvelle partie id : btnNewGame
-let newGame = document.querySelector('#btnNewGame');
+let newGame = document.getElementById('btnNewGame');
 
 // - lancer : id="btnLancer"
-let lancer = document.querySelector('#btnLancer');
+let lancer = document.getElementById('btnLancer');
 
 // - collecter id="btnCollecter"
-let collecter = document.querySelector('#btnCollecter');
+let collecter = document.getElementById('btnCollecter');
 
 
 
