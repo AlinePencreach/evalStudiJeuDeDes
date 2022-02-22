746ee8f9ce373d7e3bcf77a850caaa13ad26f33b
AlinePencreach <64145119+AlinePencreach@users.noreply.github.com>
2022-02-22 13:35:54 +0100

    redaction des commentaire pour organisation

---
+++ main.js
@@ -0,0 +1,15 @@
+/* déclarer le variable :
+- score global j1
+- score global j2
+- score joueur1
+- score joueur2
+- tour du joueur
+- resultat dés
+
+bouton pour Evenement :
+- nouvelle partie
+- lancer
+- collecter */
+
+
+
