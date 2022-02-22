4f6f9f249acbb6b0eae6044dba3eba43d7932f38
AlinePencreach <64145119+AlinePencreach@users.noreply.github.com>
2022-02-22 17:41:48 +0100

    function pour nombre aleatoire de 1 a 6 OK

--- main.js
+++ main.js
@@ -1,52 +1,9 @@
-// /* déclarer le variable :
-// joueur 1 id = "player1"
-let player1 = document.getElementById('player1');
+let number = Math.floor(Math.random() * 6) + 1;
 
-// joueur 2 id = "player2"
-let player2 = document.getElementById('player2');
+function diceRun(){
+    console.log(number);
+    let lancer = document.getElementById('btnLancer');
+    return number;
+}
 
-// - score global j1 = class = "globalPlayer"
-let global = document.getElementsByClassName('globalPlayer');
-let globalPlayer1 = 0;
-let globalPlayer2 = 0;
-
-// - score joueur1 = id = "roundPlayer1"
-let roundPlayer1 = document.getElementsById('roundPlayer1');
-
-// - score joueur2 = id = "roundPlayer2"
-let roundPlayer2 = document.getElementById('roundPlayer2');
-
-
-// - tour du joueur
-let activePlayer; //ajouter rond rouge player active
-
-// - resultat dés = diceFaces
-let diceFaces; // comment lier les imgaes des dès ???
-
-
-// bouton pour Evenement :
-
-// - nouvelle partie id : btnNewGame
-let newGame = document.getElementById('btnNewGame');
-
-// - lancer : id="btnLancer"
-let lancer = document.getElementById('btnLancer');
-
-// - collecter id="btnCollecter"
-let collecter = document.getElementById('btnCollecter');
-
-//le cercle rouge 
-let cercle = document.getElementById('cercle');
-
-
-newGame.addEventListener('click', function(){
-    globalPlayer1 = 0;
-    globalPlayer2 = 0;
-    roundPlayer1 = 0;
-    roundPlayer2 = 0;
-}) 
-    
-const lancerDeDé = function () {
-    const number = Math.random() * 6 + 1;
-     return number;
-  };
+diceRun();
