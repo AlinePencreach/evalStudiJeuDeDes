464b5f8a638f9b8e1108ac2d08f80290831d5287
AlinePencreach <64145119+AlinePencreach@users.noreply.github.com>
2022-02-22 20:20:27 +0100

    changer image en fonction du nombre random

--- main.js
+++ main.js
@@ -1,10 +1,17 @@
+// lorsqu'on click sur le bouton lancer 
 const lancer = document.getElementById("btnLancer");
 let number;
 
-lancer.addEventListener('click', function() {
+function getDiceRoll(){
     number = Math.floor(Math.random() * 6 ) + 1;
     console.log(number);
     return number;
+}
+
+lancer.addEventListener('click', function() {
+    getDiceRoll();
 });
 
+
+document.getElementById('diceFace').src = `./images/dice${number}.png`;
 
