843c32797364a12b0ab48efe204cd3b73dfa9d68
AlinePencreach <64145119+AlinePencreach@users.noreply.github.com>
2022-02-23 09:49:29 +0100

    comment changer src image

--- main.js
+++ main.js
@@ -1,21 +1,22 @@
-// lorsqu'on click sur le bouton lancer 
+//déclaration des variable du bouton lancer et number :
 const lancer = document.getElementById("btnLancer");
+const diceFace = document.getElementById('diceFace');
 let number;
 
+//fonction qui retourne un nombre entre 1 et 6 :
 function getDiceRoll(){
     number = Math.floor(Math.random() * 6 ) + 1;
     console.log(number);
     return number;
 }
 
+//  Cliquer sur le bouton lancer appel la fonction getDiceRoll qui donne un nombre aléatoire entre 1 et 6 :
 lancer.addEventListener('click', function() {
     getDiceRoll();
     diceFace;
 });
 
-
-let diceFace = document.getElementById('diceFace');
-diceFace.scr = `./images/dice${number}.png`;
+diceFace.src = "./images/dice" + number + ".png";
 
 
 
