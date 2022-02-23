f1ad3ad671062bb8b62efda7e34fcfa52a2f364a
AlinePencreach <64145119+AlinePencreach@users.noreply.github.com>
2022-02-23 15:36:24 +0100

    narrive pas afficher 0 si le dès fait 1

--- main.js
+++ main.js
@@ -9,11 +9,16 @@ let resultRound1 = 0;
 let resultRound2 = 0;
 //declaration du bouton collecter:
 const collecter = document.getElementById('btnCollecter');
-//déclarer les globalPlayer1 et globalPlayer2 a 0:
+//déclation les globalPlayer1 et globalPlayer2 a 0:
 let global1 = document.getElementById('globalPlayer1');
 let global2 = document.getElementById('globalPlayer2');
 let resultGlobal1 = 0;
 let resultGlobal2 = 0;
+//déclaration du bouton nouvelle partie :
+const newGame = document.getElementById('btnNewGame');
+//Déclaration players : 
+const player1 = document.getElementById('player1');
+const player2 = document.getElementById('player2');
 
 //déclaration de number qui est le resultat du dès lancer:
 let number;
@@ -77,4 +82,7 @@ collecter.addEventListener('click', function() {
     // stockPlayer2();
 });
 
-//Si le dès fait un on perds notre tour et nos gain de round:
+//si le dès fait 1 remise a zero du round et perds le activeplayer :
+if (number = 1) {
+    round1.textContent = 0; 
+};
