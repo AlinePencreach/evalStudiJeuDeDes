8430e54b4b314b3cf157f6b52a673fe7eed9d851
AlinePencreach <64145119+AlinePencreach@users.noreply.github.com>
2022-02-24 16:24:13 +0100

    les points du joueur 1 se stock bien dans point mais pas joueur 2

--- main.js
+++ main.js
@@ -3,9 +3,8 @@ let activePlayer;
 let numberDice;
 let resultGlobal1;
 let resultGlobal2;
-let resultRound1;
-let resultRound2;
-
+let resultRound1 = 0;
+let resultRound2 = 0;
 //declatration de selector:
 let round1 = document.getElementById('roundPlayer1');
 let round2 = document.getElementById('roundPlayer2');
@@ -18,117 +17,27 @@ const collecter = document.getElementById('btnCollecter');
 const player1 = document.getElementById('player1');
 const player2 = document.getElementById('player2');
 
-
-
-
-
-
-
-// //fonction qui retourne un nombre entre 1 et 6 :
-// function getDiceRoll(){
-//     number = Math.floor(Math.random() * 6 ) + 1;
-//     console.log(number);
-//     return number;
-// };
-
-
-// //fonction qui change la src de l'image en fonction de number : 
-// function imgDiceRoll() {
-//     diceFace.src = "./images/dice" + number + ".png";
-//     console.log(diceFace);
-// };
-
-// //Evenement au click sur le bouton lancer les fonctions getDiceRoll et imgDiceRoll se lance. Retourne un nombre aléatoir et l'image correspondante :
-// lancer.addEventListener('click', function() {
-//     getDiceRoll();
-//     imgDiceRoll();
-//     stockRound1();
-//     // stockRound2();
-// });
-
-
-// //Récuperer number et l'afficher dans point du joueur soit roundPlayer1 ou roundPlayer2 selon le joueur active :
-// //JOUEUR1
-// function stockRound1(){
-//     resultRound1 = resultRound1 + number;
-//     round1.innerHTML = resultRound1;
-//     return resultRound1;
-// }
-
-// //JOUEUR2
-// function stockRound2(){
-//     resultRound2 = resultRound2 + number;
-//     round2.innerHTML = resultRound2;
-//     return resultRound2;
-// }
-
-
-// //Lorsqu'on click sur collecter le resultRound va dans globalPlayer1 et 2:
-// function stockPlayer1(){
-//     resultGlobal1 = resultGlobal1 + resultRound1;
-//     global1.innerHTML = resultGlobal1;
-//     resultRound1 = 0;
-//     round1.innerHTML = resultRound1;
-//     return resultGlobal1;
-// }
-
-// function stockPlayer2(){
-//     resultGlobal2 = resultGlobal2 + resultRound2;
-//     global2.innerHTML = resultGlobal2;
-//     resultRound2 = 0;
-//     round2.innerHTML = resultRound2;
-//     return resultGlobal1;
-// }
-
-// //ajout de l'event click sur le btnCollecter : 
-// collecter.addEventListener('click', function() {
-//     stockPlayer1();
-//     stockPlayer2();
-// });
-
-
-// //Foction que change le jouer
-// function switchPlayer() {
-//     if (activePlayer === 'player 1') {
-//       activePlayer = 'player 2';
-//     } else {
-//       activePlayer = 'player 1';
-//     }
-// }
-
 //REFONTE PAS À PAS :
+
 //CREATION DE LA FONCTION QUI RETOURNE UN NOMBRE ALEATOIRE ENTRE 1 ET 6 :
 function randomNumberDice(){
     numberDice = Math.floor(Math.random() * 6 ) + 1;
     console.log(numberDice);
     diceFace.src = "./images/dice" + numberDice + ".png";
+    console.log(diceFace);
     return numberDice;
 }
-
-//RETOURNER LE NUMBERDICE DANS LA CASE ROUND:
-function stockRound(){
-    if (activePlayer === 'player1') {
-        resultRound1 = resultRound1 + numberDice;
-        round1 = resultRound1;
-        console.log(round1);
-        return resultRound1;
-    } else {
-        resultRound2 = resultRound2 + numberDice;
-        round2 = resultRound2;
-        return resultRound2;
-    }   
-};
 
 //FONCTION QUI PERMET DE SWITCHER DE JOUEUR:
 function switchPlayer(){
     if (activePlayer === 'player1') {
         activePlayer = 'player2';
+        console.log(activePlayer);
     } else {
         activePlayer = 'player1';
+        console.log(activePlayer);
     }
-    console.log(activePlayer);
-}
-
+};
 
 //ACTIVER UN JOUEUR LORSQU'ON CLICK SUR LE BOUTON NOUVELLE PARTIE ET REMETTRE LA PARTIE A ZERO :
 function startGame(){
@@ -140,7 +49,6 @@ function startGame(){
     console.log('Vous venez de cliquer sur le bouton nouvelle partie');
     console.log(activePlayer);
 }
-
 
 //FONCTION QUI PERMET DE METTRE LE POINT ROUGE SUR LE JOUEUR ACTIF:
 function activePlayerStyle(){
@@ -173,22 +81,61 @@ function activePlayerStyle(){
     } 
 } 
 
+//FONCTION QUI STOCK LES POINTS :
+// function roundPlayer1(){
+//     resultRound1 = resultRound1 + numberDice;
+//     round1.innerHTML = resultRound1;
+//     console.log(resultRound1);
+//     return resultRound1;
+// };
+
+// function roundPlayer2(){
+//     resultRound2 = resultRound2 + numberDice;
+//     round1.innerHTML = resultRound2;
+//     console.log(resultRound2);
+//     return resultRound2;
+// };
+
+function roundTourPlayer(){
+    if (activePlayer === 'player1') {
+        resultRound1 = resultRound1 + numberDice;
+        round1.innerHTML = resultRound1;
+        console.log(resultRound1);
+        return resultRound1;
+    } else if (activePlayer === 'player2'){
+        resultRound2 = resultRound2 + numberDice;
+        round1.innerHTML = resultRound2;
+        console.log(resultRound2);
+        return resultRound2;
+    }
+}
 
 
+function looseDiceOne(){
+    if (numberDice === 1) {
+        if (activePlayer === 'player1') {
+            activePlayer = 'player2';
+            console.log(activePlayer);
+            return resultRound1 = 0;
+        } else {
+            activePlayer = 'player1';
+            console.log(activePlayer);
+            return resultRound2 = 0;
+        }
+    }
+}
 
-    
-
-
+//APPEL DES LISTENER :
 newGame.addEventListener('click', function(){
     startGame();
     activePlayerStyle();
 });
-
 lancer.addEventListener('click', function(){
     randomNumberDice();
-    stockRound();
+    roundTourPlayer();
+    looseDiceOne()
+    activePlayerStyle();
 })
-
 collecter.addEventListener('click', function(){
     switchPlayer();
     activePlayerStyle();
